0f5e2075c6aacef3c72840ef440bc2c0147f94ec
Ankit Chahar <ankitchahar11@gmail.com>
2017-08-31 03:32:38 +0530

    Update and rename Template to Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -4,6 +4,7 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 #define REP(i,a,b) for(int i=a;i<b;i++)
 #define REPR(i,a,b) for(int i=b;i>=a;i--)
+#define show(a) for(int i=0;i<a.size();i++){cout<<a[i]<<" ";}
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
