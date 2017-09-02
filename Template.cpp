2b0368e3ed7ef9b9904a562028103a61994ce6ea
Ankit Chahar <ankitchahar11@gmail.com>
2017-09-02 20:57:42 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -5,6 +5,8 @@ typedef vector<vector<int>> vvi;
 #define REP(i,a,b) for(int i=a;i<b;i++)
 #define REPR(i,a,b) for(int i=b;i>=a;i--)
 #define show(a) for(int i=0;i<a.size();i++){cout<<a[i]<<" ";}
+#define show2(a) REP(i,0,a.size()){REP(j,0,a[i].size()){cout<<a[i][j]<<" ";}cout<<endl;}
+
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
