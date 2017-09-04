8b91b864cd6acef0b45aef5b418904d3529c3c47
Ankit Chahar <ankitchahar11@gmail.com>
2017-09-04 20:26:06 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -6,10 +6,17 @@ typedef vector<vector<int>> vvi;
 #define REPR(i,a,b) for(int i=b;i>=a;i--)
 #define show(a) for(int i=0;i<a.size();i++){cout<<a[i]<<" ";}
 #define show2(a) REP(i,0,a.size()){REP(j,0,a[i].size()){cout<<a[i][j]<<" ";}cout<<endl;}
+#define pb push_back
+#define mp make_pair
+#define min(a,b) (a<b?a:b)
+#define min2(a,b,c) (a<b?(a<c?a:c):(b<c?b:c))
+#define max(a,b) (a>b?a:b)
+#define max2(a,b,c) (a>b?(a>c?a:c):(b>c?b:c))
 
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 	
 	return 0;
 }
